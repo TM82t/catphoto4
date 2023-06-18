a00a8a13a22ab7a1d6b2dcd34279a3edacbaac1a
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-06-18 12:25:16 +0000

    [Add]routing

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,2 +1,29 @@
 class Admin::EndUsersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :ensure_end_user, only: [:show, :edit, :update]
+  
+  def index
+    @end_users = EndUser.page(params[:page])
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    @end_user.update(end_user_params) ? (redirect_to admin_end_user_path(@end_user)) : (render :edit)
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name, :email, :postal_code, :address, :telephone_number, :is_active)
+  end
+
+  def ensure_end_user
+    @end_user = EndUser.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,39 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions',
   }
 
-  devise_for :end_users, controllers: {
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :end_user, only: [:index, :show, :edit, :update]
+  end
+
+  devise_for :end_users, skip: [:passwords], controllers: {
     sessions: 'public/sessions',
     registrations: 'public/registrations',
   }
 
   scope module: :public do
-   root to: 'homes#top'
+    root to: 'homes#top'
+    get 'end_users/mypage' => 'end_users#show', as: 'mypage'
+    # end_users/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+    get 'end_users/information/edit' => 'end_useres#edit', as: 'edit_information'
+    patch 'end_users/information' => 'end_users#update', as: 'update_information'
+    get 'end_users/unsubscribe' => 'end_users#unsubscribe', as: 'confirm_unsubscribe'
+    put 'end_users/information' => 'end_users#update'
+    patch 'end_users/withdraw' => 'end_users#withdraw', as: 'withdraw_customer'
+
+    resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
+     resources :post_comments, only: [:create, :destroy]
+     resource :favorites, only: [:create, :destroy]
+    end
+
+    #通知
+    resources :post_comment_notifications, only: :index
+    resources :favorite_notifications, only: :index
+    resources :follow_notigicatinos, only: :index
+    #ユーザー･投稿検索
+    get 'search' => 'users#search'
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
